f7810f1ac2591e88c508c53eb0fe7312c4f5b5ba
Bob Moore <bob.moore@midwestdigitalmarketing.com>
2021-02-15 10:25:38 -0600

    Updated BBDK Buttons

--- wp-content/plugins/bb-dev-kit/modules/button/includes/frontend.css.php
+++ wp-content/plugins/bb-dev-kit/modules/button/includes/frontend.css.php
@@ -3,23 +3,23 @@
 \FLBuilderCSS::typography_field_rule([
 	'settings' => $settings,
 	'setting_name' => 'typography',
-	'selector' => ".fl-node-{$id} a.bbdk-button",
+	'selector' => ".fl-builder-content .fl-node-{$id} a.bbdk-button",
 ] );
 
 \FLBuilderCSS::border_field_rule( array(
 	'settings' => $settings,
 	'setting_name' => 'border',
-	'selector' => ".fl-node-$id a.bbdk-button",
+	'selector' => ".fl-builder-content .fl-node-$id a.bbdk-button",
 ) );
 
 \FLBuilderCSS::border_field_rule( array(
 	'settings' => $settings,
 	'setting_name' => 'border_hover',
-	'selector' => ".fl-node-$id a.bbdk-button:hover, .fl-node-$id a.bbdk-button:focus",
+	'selector' => ".fl-builder-content .fl-node-$id a.bbdk-button:hover, .fl-node-$id a.bbdk-button:focus",
 ) );
 
 echo $module->moduleCss([
 	'module' => $module,
-	'selector' => ".fl-node-{$id}",
+	'selector' => ".fl-builder-content .fl-node-{$id}",
 ]);
 
