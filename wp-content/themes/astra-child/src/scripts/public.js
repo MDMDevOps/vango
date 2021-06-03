554046e978258d48dc74625dee8ed3ced9a3ba56
Bob Moore <bob.moore@midwestdigitalmarketing.com>
2021-06-03 10:38:58 -0500

    Added 2 day advance for datepicker on book now form

--- wp-content/themes/astra-child/src/scripts/public.js
+++ wp-content/themes/astra-child/src/scripts/public.js
@@ -64,68 +64,17 @@ document.addEventListener( 'DOMContentLoaded', _scrollToggle, false );
 jQuery( function ( $ ) {
 	'use strict';
 
-	let $sortlist = $.map( $( '.list-sort' ), function( el ) {
-		return new ListSort( $( el ) );
-	});
-
-	function ListSort( $el ) {
-
-		let $target, reload, active_class, $buttons, $items;
-
-		const _toggle = ( event ) => {
-			event.preventDefault();
-
-			for( let i = 0; i < $buttons.length; i++ ) {
-				$buttons[i].removeClass( 'active' );
+	if ( typeof gform === 'object' ) {
+		gform.addFilter( 'gform_datepicker_options_pre_init', function( optionsObj, formId, fieldId ) {
+			/**
+			 * Require 2 days in advance
+			 */
+			if ( formId == 3 && fieldId == 6 ) {
+				optionsObj.minDate = '+2';
 			}
-
-			event.data.button.addClass( 'active' )
-
-			for( let i = 0; i < $items.length; i++ ) {
-
-				if( event.data.target === true ) {
-					$items[i].addClass( 'active' ).removeClass( 'inactive' );
-				}
-				else {
-					if( $items[i].hasClass( event.data.target ) ) {
-						$items[i].addClass( active_class ).removeClass( 'inactive' );
-					}
-					else {
-						$items[i].addClass( 'inactive' ).removeClass( active_class );
-					}
-				}
-			}
-
-			document.dispatchEvent( reload );
-		}
-
-
-		$target = $( '#' + $el.data( 'target-list' ) );
-
-		if( $target.length ) {
-
-			reload = new CustomEvent( 'list-sort-reload' );
-
-			active_class = $el.data( 'masonry-tweak' ) == '1' ? 'active masonry_column' : 'active';
-
-			$buttons = $.map( $el.find( 'a' ), function( button ) {
-
-				let $button = $( button );
-
-				$button.on( 'click', { 'target' : $button.data( 'sort-class' ), 'button' : $button },_toggle );
-
-				return $button;
-
-			});
-
-			$items = $.map( $( '.' + $el.data( 'item-class' ) ), function( item ) {
-
-				let $item = $( item );
-
-				return $item;
-			});
-
-		}
-
+			return optionsObj;
+		} );
 	}
+
+
 });
